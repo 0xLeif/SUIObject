f82d7e33682566c82ef102a9356bf90f9742d41d
zmeriksen <zmeriksen@icloud.com>
2020-09-09 20:57:29 -0500

    Updated to match Object 0.5.0

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "SUIObject",
+    name: "Object",
     platforms: [
         .iOS(.v13),
         .macOS(.v10_15),
@@ -14,7 +14,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "SUIObject",
+            name: "Object",
             targets: ["SUIObject"]),
     ],
     dependencies: [
